80c171e616146cf0095dd59ce63d9cd985475f9e
vickydasta <vickydasta@gmail.com>
2017-01-14 14:36:58 +0700

    initial commit

---
+++ datstruct.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+integer = 1
+
+float = 3.14
+
+string = "hello, world!"
+
+list = ["apple", "orange"]
+
+dictionary = {"a": 1, "b": 2, "c": 3}
+
+tupple = (12, 13, 14)
+
+print integer
+print float
+print string
+print list
+print dictionary
+print tupple
+
+
+
